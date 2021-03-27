7583e015228d76bb9120cec5faf6c57961c3891b
Diego Rubio <darubiomunoz@gmail.com>
2021-03-27 18:10:28 -0300

    global variables for the project

---
+++ src/Variables.js
@@ -0,0 +1,43 @@
+export const colorOption1 = `hsl(209, 23%, 22%)`;
+export const colorOption2 = `hsl(207, 26%, 17%)`;
+export const colorOption3 = `hsl(200, 15%, 8%)`;
+export const colorOption4 = `hsl(0, 0%, 52%)`;
+export const colorOption5 = `hsl(0, 0%, 98%)`;
+export const colorOption6 = `hsl(0, 0%, 100%)`;
+export const colorOption7 = `#FFF`;
+export const colorOption8 = `#000`;
+
+export const fontFamily = `'Nunito Sans', sans-serif`;
+
+export const fontSizeSmall = `10px`;
+export const fontSizeRegular = `12px`;
+export const fontSizeNormal = `16px`;
+export const fontSizeMedium = `24px`;
+export const fontSizeLarge = `32px`;
+export const fontSizeExtraLarge = `48px`;
+
+export const fontWeightExtraLight = `200`;
+export const fontWeightLight = `300`;
+export const fontWeightNormal = `400`;
+export const fontWeightBold = `700`;
+export const fontWeightExtraBold = `900`;
+
+export const breakpointMobile = `375px`;
+export const breakpointTablet = `600px`;
+export const breakpointDesktop = `1024px`;
+
+export const spacingSmall = `10px`;
+export const spacingRegular = `20px`;
+export const spacingNormal = `30px`;
+export const spacingLarge = `40px`;
+export const spacingExtraLarge = `50px`;
+
+export const zIndexBackground = `0`;
+export const zIndexNormal = `10`;
+export const zIndexDropDown = `1000`;
+export const zIndexSticky = `1010`;
+export const zIndexFixed = `1020`;
+export const zIndexModalBackDrop = `1030`;
+export const zIndexModal = `1040`;
+export const zIndexPopOver = `1050`;
+export const zIndexToolTip = `1060`;
