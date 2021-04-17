ffd423348b4a3d01d2b05cc7867b4e192b09e849
Diego Rubio <darubiomunoz@gmail.com>
2021-04-16 21:33:24 -0400

    Creating the pages' files and setting the app file with react router

---
+++ src/routes/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import './App.css';
+
+import HomePage from '../pages/HomePage';
+import Country from '../pages/Country';
+import NotFound from '../pages/NotFound';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/country/id:" component={Country} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
