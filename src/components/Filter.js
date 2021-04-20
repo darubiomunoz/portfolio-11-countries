db419c9f21a5e7d4c4d8928d1ed09b941a128866
Diego Rubio <darubiomunoz@gmail.com>
2021-04-20 17:40:04 -0400

    Adding value attributes to the radio inputs

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -8,15 +8,35 @@ const Filter = () => {
     <StyledFilterSection>
       <StyledFilterDiv>
         <StyledLabel for="Africa">Africa</StyledLabel>
-        <StyledRadioInput id="Africa" name="category" type="radio" />
+        <StyledRadioInput
+          id="Africa"
+          value="Africa"
+          name="category"
+          type="radio"
+        />
         <StyledLabel for="America">America</StyledLabel>
-        <StyledRadioInput id="America" name="category" type="radio" />
+        <StyledRadioInput
+          id="America"
+          value="America"
+          name="category"
+          type="radio"
+        />
         <StyledLabel for="Asia">Asia</StyledLabel>
-        <StyledRadioInput id="Asia" name="category" type="radio" />
+        <StyledRadioInput id="Asia" value="Asia" name="category" type="radio" />
         <StyledLabel for="Europe">Europe</StyledLabel>
-        <StyledRadioInput id="Europe" name="category" type="radio" />
+        <StyledRadioInput
+          id="Europe"
+          value="Europe"
+          name="category"
+          type="radio"
+        />
         <StyledLabel for="Oceania">Oceania</StyledLabel>
-        <StyledRadioInput id="Oceania" name="category" type="radio" />
+        <StyledRadioInput
+          id="Oceania"
+          value="Oceania"
+          name="category"
+          type="radio"
+        />
       </StyledFilterDiv>
     </StyledFilterSection>
   );
