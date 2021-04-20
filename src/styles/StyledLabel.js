743639cee76b5800e6927ab0a0856bf95f9707bb
Diego Rubio <darubiomunoz@gmail.com>
2021-04-20 18:18:13 -0400

    Filter section styles done

--- src/styles/StyledLabel.js
+++ src/styles/StyledLabel.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 import { colorOption1, fontSizeSmall } from './Variables';
 
 const StyledLabel = styled.label`
-  width: 80%;
+  width: 100%;
+  text-align: start;
   font-size: ${fontSizeSmall};
   color: ${colorOption1};
   cursor: pointer;
