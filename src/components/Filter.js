dde6741c3d8c7816d259bf9cc43cd66bdec2db54
Diego Rubio <darubiomunoz@gmail.com>
2021-04-20 17:37:09 -0400

    Adding the radio input faking an option label

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,16 +1,22 @@
 import StyledFilterSection from '../styles/StyledFilterSection';
 import StyledFilterDiv from '../styles/StyledFilterDiv';
 import StyledLabel from '../styles/StyledLabel';
+import StyledRadioInput from '../styles/StyledRadioInput';
 
 const Filter = () => {
   return (
     <StyledFilterSection>
       <StyledFilterDiv>
         <StyledLabel for="Africa">Africa</StyledLabel>
+        <StyledRadioInput id="Africa" name="category" type="radio" />
         <StyledLabel for="America">America</StyledLabel>
+        <StyledRadioInput id="America" name="category" type="radio" />
         <StyledLabel for="Asia">Asia</StyledLabel>
+        <StyledRadioInput id="Asia" name="category" type="radio" />
         <StyledLabel for="Europe">Europe</StyledLabel>
+        <StyledRadioInput id="Europe" name="category" type="radio" />
         <StyledLabel for="Oceania">Oceania</StyledLabel>
+        <StyledRadioInput id="Oceania" name="category" type="radio" />
       </StyledFilterDiv>
     </StyledFilterSection>
   );
---
+++ src/styles/StyledRadioInput.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledRadioInput = styled.input`
+  display: none;
+`;
+
+export default StyledRadioInput;
