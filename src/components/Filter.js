c41e5f1522f4f6028a8aaa16790e92b32d9b85a8
Diego Rubio <darubiomunoz@gmail.com>
2021-04-20 16:27:09 -0400

    Adding a container for the div in the filter component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,12 @@
 import StyledFilterSection from '../styles/StyledFilterSection';
+import StyledFilterDiv from '../styles/StyledFilterDiv';
 
 const Filter = () => {
   return (
     <StyledFilterSection>
-      
+      <StyledFilterDiv>
+
+      </StyledFilterDiv>
     </StyledFilterSection>
   );
 }
---
+++ src/styles/StyledFilterDiv.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import StyledDiv from './StyledDiv';
+
+import { colorOption4, colorOption5, spacingNormal, spacingExtraSmall } from './Variables'
+
+const StyledFilterDiv = styled(StyledDiv)`
+  width: 50%;
+  padding: 0 ${spacingExtraSmall};
+`;
+
+export default StyledFilterDiv;
