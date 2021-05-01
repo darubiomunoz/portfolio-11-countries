360af8c0d5862e2d7af27325f72399fb93b28b68
Diego Rubio <darubiomunoz@gmail.com>
2021-05-01 16:36:26 -0400

    Changing the region on the filter button

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,50 +1,69 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import StyledFilterSection from '../styles/components/filter/StyledFilterSection';
-import StyledFilterDiv from '../styles/components/filter/StyledFilterDiv';
-import StyledLabel from '../styles/components/filter/StyledLabel';
-import StyledRadioInput from '../styles/components/filter/StyledRadioInput';
-import StyledFilterButton from '../styles/components/filter/StyledFilterButton';
-import StyledFilterIcon from '../styles/components/filter/StyledFilterIcon';
+import StyledFilterSection from "../styles/components/filter/StyledFilterSection";
+import StyledFilterDiv from "../styles/components/filter/StyledFilterDiv";
+import StyledLabel from "../styles/components/filter/StyledLabel";
+import StyledRadioInput from "../styles/components/filter/StyledRadioInput";
+import StyledFilterButton from "../styles/components/filter/StyledFilterButton";
+import StyledFilterIcon from "../styles/components/filter/StyledFilterIcon";
 
 const Filter = () => {
   const [open, setOpen] = useState(false);
+  const [filter, setFilter] = useState("Filter by Region");
 
   const handleClick = () => {
     setOpen(!open);
-  }
+  };
+
+  const handleFiltering = (event) => {
+    setFilter(event.target.htmlFor);
+    setOpen(!open);
+  };
 
   return (
     <StyledFilterSection>
       <StyledFilterButton onClick={handleClick}>
-        Filter by Region
-        <StyledFilterIcon className={`fas fa-angle-${open ? 'up' : 'down'}`} />
+        {filter}
+        <StyledFilterIcon className={`fas fa-angle-${open ? "up" : "down"}`} />
       </StyledFilterButton>
-      <StyledFilterDiv invisible={open ? '' : 'invisible'} >
-        <StyledLabel htmlFor="Africa">Africa</StyledLabel>
+      <StyledFilterDiv invisible={open ? "" : "invisible"}>
+        <StyledLabel
+          htmlFor="Africa"
+          onClick={(event) => handleFiltering(event)}
+        >
+          Africa
+        </StyledLabel>
         <StyledRadioInput
           id="Africa"
           value="Africa"
           name="category"
           type="radio"
         />
-        <StyledLabel htmlFor="America">America</StyledLabel>
+        <StyledLabel htmlFor="America" onClick={handleFiltering}>
+          America
+        </StyledLabel>
         <StyledRadioInput
           id="America"
           value="America"
           name="category"
           type="radio"
         />
-        <StyledLabel htmlFor="Asia">Asia</StyledLabel>
+        <StyledLabel htmlFor="Asia" onClick={handleFiltering}>
+          Asia
+        </StyledLabel>
         <StyledRadioInput id="Asia" value="Asia" name="category" type="radio" />
-        <StyledLabel htmlFor="Europe">Europe</StyledLabel>
+        <StyledLabel htmlFor="Europe" onClick={handleFiltering}>
+          Europe
+        </StyledLabel>
         <StyledRadioInput
           id="Europe"
           value="Europe"
           name="category"
           type="radio"
         />
-        <StyledLabel htmlFor="Oceania">Oceania</StyledLabel>
+        <StyledLabel htmlFor="Oceania" onClick={handleFiltering}>
+          Oceania
+        </StyledLabel>
         <StyledRadioInput
           id="Oceania"
           value="Oceania"
@@ -54,6 +73,6 @@ const Filter = () => {
       </StyledFilterDiv>
     </StyledFilterSection>
   );
-}
+};
 
 export default Filter;
