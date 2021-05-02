5c97b7b6864eb09864f6a20f58005b7c4775d7f4
Diego Rubio <darubiomunoz@gmail.com>
2021-05-01 22:41:13 -0400

    Adding the reducer of the search slice in the store

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import countriesReducer from "../features/countries/countriesSlice";
 import filterReducer from "../features/filter/filterSlice";
+import searchReducer from "../features/search/searchSlice";
 
 export default configureStore({
   reducer: {
     countries: countriesReducer,
     filter: filterReducer,
+    search: searchReducer,
   },
 });
