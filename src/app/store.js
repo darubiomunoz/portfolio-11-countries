cec6b95c841ff2d78de9bb3ea3619bbb0edd449c
Diego Rubio <darubiomunoz@gmail.com>
2021-05-02 19:14:55 -0400

    Adding the theme reducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import countriesReducer from "../features/countries/countriesSlice";
 import filterReducer from "../features/filter/filterSlice";
 import searchReducer from "../features/search/searchSlice";
+import themeReducer from "../features/theme/themeSlice";
 
 export default configureStore({
   reducer: {
     countries: countriesReducer,
     filter: filterReducer,
     search: searchReducer,
+    theme: themeReducer,
   },
 });
