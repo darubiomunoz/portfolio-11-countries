641a6357335e6435f19cb4b082a141de599a0094
Diego Rubio <darubiomunoz@gmail.com>
2021-05-05 00:04:10 -0400

    Creating useFormatNumber custom hook

---
+++ src/hooks/useFormatNumber.js
@@ -0,0 +1,15 @@
+const formatNumber = (string) => {
+  string += "";
+  var x = string.split(".");
+  var x1 = x[0];
+  var x2 = x.length > 1 ? `${"." + x[1]}` : "";
+  var regex = /(\d+)(\d{3})/;
+
+  while (regex.test(x1)) {
+    x1 = x1.replace(regex, "$1" + "," + "$2");
+  }
+
+  return x1 + x2;
+};
+
+export default formatNumber;
